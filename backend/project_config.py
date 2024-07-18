af1713fedae8702a3d67980e9fb41b8078a9b706
Daniel Broe <dbroe1995@gmail.com>
2024-07-18 13:32:24 -0400

    Fix incorrect environment variable name

--- backend/project_config.py
+++ backend/project_config.py
@@ -3,7 +3,7 @@ from dotenv import load_dotenv
 
 def setup_openai_config():
     import openai
-    openai.api_key = os.getenv('OPEN_API_KEY')
+    openai.api_key = os.getenv('OPENAI_API_KEY')
     openai.organization = os.getenv('OPENAI_ORG_ID')
 
 def setup_app_config():
